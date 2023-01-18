0b83bec7e31b381354dd06ac56e1b7e2cb38c812
HoangKin00 <haudaothi60@gmail.com>
2023-01-18 10:41:03 +0700

    update2

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -5,24 +5,54 @@ class Promotion {
         this.$main.innerHTML =
             `
     <div class="screen6__pic">
-    <img src="assets/images/page6-bg.jpg?ver=7" alt="">
-</div>
+        <picture>
+            <source media="(min-width: 465px)" srcset="assets/images/slide-dt.jpg">
+            <img src="assets/images/slide-mb.jpg">
+        </picture>
+    </div>
+    <section class="screen7">
+            <div class="screen7__list">
+                <div class="screen7__item screen7__item6">
+                    <img src="assets/images/lixi6.png" alt="">
+                </div>
+                    <div class="screen7__item screen7__item5">
+                        <img src="assets/images/lixi5.png" alt="">
+                    </div>
+                    <div class="screen7__item screen7__item4">
+                        <img src="assets/images/lixi4.png" alt="">
+                    </div>
+            </div>
+            <div class="screen7__box">
+                <div class="screen7__item screen7__item2">
+                    <img src="assets/images/lixi2.png" alt="">
+                </div>
+                <div class="screen7__item screen7__item1">
+                    <img src="assets/images/lixi1.png" alt="">
+                </div>
+                <div class="screen7__item screen7__item3">
+                    <img src="assets/images/lixi3.png" alt="">
+                </div>
+            </div>
+            <div class="screen7__note">
+                100% đến Kangnam <br> là có lộc
+            </div>
+    </section>
 <div class="screen6__box1">
     <div class="container">
         <div class="screen6__des">
-            QUÀ SIÊU TO MỪNG SINH NHẬT
+        SALE LỚN NHẤT NĂM
         </div>
         <div class="screen6__text">
-            <p><b>1. TẶNG 999 SUẤT SALE TO SINH NHẬT</b></p>
-            <p><li><b>Tặng 1 Triệu:</b> Cắt mí 6D/ Perfect Eyes/ Nâng mũi cấu trúc 4D/ Giảm mỡ Vaser 4D toàn bụng/Thẩm mỹ môi 6D/ Trẻ hóa Mi Young (mới)</li></p>
-            <p><li><b>Tặng 2 Triệu:</b>Nâng mũi 6D/ Nâng mũi sụn sườn các loại/ Nâng ngực 6D/6D VIP/ Hút mỡ 6D tia sương toàn bụng/ Trị sẹo Scar Nano từ 4 buổi/ Trị nám liệu trình từ 6 buổi</li> </p>
-            <p><li><b>Tặng 3 Triệu:</b>Khách hàng thực hiện hàm mặt (Chỉnh hàm hô móm / Hạ gò má/ Gọt hàm/ combo hàm mặt các loại)</li></p>
-            <p><b>2. VOUCHER LAN TỎA </b></p>
-            <p><li>Tặng <b>Voucher 300K</b> khi check – in hoặc livestream chia sẻ về dịch vụ tiểu phẫu/ Laser & Spa.</li></p>
-            <p><li>Tặng <b>Voucher 1.000.000đ</b> khách hàng chia sẻ cảm nhận liên quan đến dịch vụ đại phẫu, cho phép lộ mặt (Nâng ngực – Hút mỡ)</li></p>
-            <p><li>Minigame tại cơ sở tặng bộ mỹ phẩm cao cấp, vàng, bông tai vàng…</li></p>
-            <p><b>3. CƠ HỘI TRÚNG XE SH</b></p>
-            <p><li>Cơ hội trúng xe SH trị giá hơn <b>100 triệu</b> cho khách hàng có hóa đơn từ 10 triệu</li></p>
+            <p><b>1. COMBO ĐẸP ĐỘT PHÁ</b></p>
+            <p><li>Khi Nâng ngực/ Hút mỡ: <b>Giảm thêm 3%</b> cho Nâng mũi/ Cắt mí/ Thẩm mỹ môi.</li></p>
+            <p><li>Khi Nâng mũi: <b>Giảm thêm 3%</b> cho Cắt mí/ Thẩm mỹ môi.</li></p>
+            <p><b>2. ƯU ĐÃI ĐẸP TOÀN DIỆN</b></p>
+            <p><li>Tẩy nốt ruồi mức 1 chỉ với giá <b>99.000đ</b></li></p>
+            <p><li>Cạo vôi và đánh bóng răng mức 1, mức 2 – <b>đồng giá 150.000đ</b></li></p>
+            <p><li><b>Giảm 40% Phun xăm </b>khi kết hợp PTTM.</li></p>
+            <p><li><b>Giảm 50% </b>Tẩy trắng răng Laser Whitening/ Trám răng thẩm mỹ/ Nhổ răng cho khách hàng PTTM.</li></p>
+            <p><li><b>Giảm thêm 5% </b>Cấy mỡ toàn mặt / Căng chỉ toàn mặt / Trẻ hóa Mi Young khi thực hiện tiểu/ đại phẫu.</li></p>
+            <p><li><b>Giảm thêm 5% </b>Treo sa trễ/ Tạo hình vú/ Nâng ngực mỡ tự thân khi hút mỡ toàn bụng.</li></p>
         </div>
     </div>
 </div>
@@ -63,7 +93,7 @@ class Promotion {
                 <tr>
                     <td>Trẻ hóa Mi Young</td>
                     <td>-25%</td>
-                    <td>32.25 - 65.25 Triệu</td>
+                    <td>35.25 - 65.25 Triệu</td>
                 </tr>
                 <tr>
                     <td>Nâng ngực K-nature 4D</td>
