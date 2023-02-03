ca9de1b7edc45e34da4db312b7c596b18c1b5793
HoangKin00 <haudaothi60@gmail.com>
2023-02-03 14:05:15 +0700

    updatem6

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -6,62 +6,23 @@ class Promotion {
             `
     <div class="screen6__pic">
         <picture>
-            <source media="(min-width: 465px)" srcset="assets/images/slide-dt.jpg">
-            <img src="assets/images/slide-mb.jpg">
+            <source media="(min-width: 465px)" srcset="assets/images/page1-bg.jpg">
+            <img src="assets/images/page1-bg-mb.jpg">
         </picture>
     </div>
-    <section class="screen7">
-            <div class="screen7__list">
-                <div class="screen7__item screen7__item6">
-                    <img src="assets/images/lixi6.png" alt="">
-                </div>
-                    <div class="screen7__item screen7__item5">
-                        <img src="assets/images/lixi5.png" alt="">
-                    </div>
-                    <div class="screen7__item screen7__item4">
-                        <img src="assets/images/lixi4.png" alt="">
-                    </div>
-            </div>
-            <div class="screen7__box">
-                <div class="screen7__item screen7__item2">
-                    <img src="assets/images/lixi2.png" alt="">
-                </div>
-                <div class="screen7__item screen7__item1">
-                    <img src="assets/images/lixi1.png" alt="">
-                </div>
-                <div class="screen7__item screen7__item3">
-                    <img src="assets/images/lixi3.png" alt="">
-                </div>
-            </div>
-            <div class="screen7__note">
-                100% đến Kangnam <br> là có lộc
-            </div>
-    </section>
 <div class="screen6__box1">
     <div class="container">
         <div class="screen6__des">
-        SALE LỚN NHẤT NĂM
+        HỘI XUÂN THẨM MỸ
         </div>
         <div class="screen6__text">
-            <p><b>1. KHAI XUÂN – HÁI LỘC TRÚNG VÀNG 9999 </b></p>
-            <p><li><b>10 Chỉ vàng 9999</b></li></p>
-            <p><li><b>5 Nhẫn</b> Kim tiền 18K </li></p>
-            <p><li><b>10 Voucher</b> 50% thẩm mỹ, lựa chọn 1 trong các dịch vụ sau: (Nâng mũi Sline 3D/ Cấu trúc 4D/ Cắt mí Plasma/ Thu mỏng môi/ Tạo môi tim/ Nâng ngực K-Nature 4D/ Giảm mỡ 4D/ Trẻ hóa Mi Young (mỗi cơ sở 1 suất) </li></p>
-            <p><li>299 <b>Lì xì 100K – 200K</b> cho hóa đơn từ 1TR </li></p>
-            <p><li>100 Voucher <b>Phun xăm -50%</b></li></p>
-            <p><li>100 Voucher chăm sóc nha chu <b>EMS mức 1</b></li></p>
-            <p><li>80 Voucher chăm sóc da miễn phí trị giá tới <b>2 Triệu </b></li></p>
-            <p><i>* Khách hàng trúng suất -50% cần đồng ý cho quay chụp dữ liệu, cho phép sử dụng hình ảnh truyền thông (trước – trong và sau dịch vụ)</i></p>
-            <p><i>* Trường hợp khách không đồng ý, cho phép quay lại.</i></p>
-            <p><b>2. COMBO SIÊU ƯU ĐÃI </b></p>
-            <p><li>Khi Nâng ngực/ Hút mỡ: <b>Giảm thêm 3%</b> cho nâng mũi/ Cắt mí/ Thẩm mỹ môi</li></p>
-            <p><li>Khi Nâng mũi: <b>Giảm thêm 3%</b> cho Cắt mí/ Thẩm mỹ môi</li></p>
-            <p><i>* Không áp dụng cộng dồn với chương trình đặt cọc trước </i></p>
-            <p><b>3. TRI ÂN KHÁCH HÀNG PTTM </b></p>
-            <p><li><b>Giảm 40% </b>Phun xăm khi kết hợp PTTM</li></p>
-            <p><li><b>Giảm 50% </b>Tẩy trắng răng Laser Whitening/ Trám răng thẩm mỹ/ Nhổ răng cho khách hàng PTTM</li></p>
-            <p><li><b>Giảm thêm 5% </b>Cấy mỡ toàn mặt / Căng chỉ toàn mặt / Trẻ hóa Mi Young khi thực hiện tiểu phẫu, đại phẫu</li></p>
-            <p><li>Tặng 1 buổi chăm sóc da <b>miễn phí</b> (điện di C/ điện di cho khách hàng đăng ký dịch vụ VIP)</li></p>
+            <p><b>1. ĐI CÀNG NHIỀU – GIẢM CÀNG SÂU </b></p>
+            <p><li>Giảm thêm 3% cho dịch vụ Nâng mũi/ Cắt mí khi đi nhóm 2 người</li></p>
+            <p><b>2. TRI ÂN KHÁCH HÀNG PTTM </b></p>
+            <p><li><b>Giảm 40% Phun xăm</b> khi kết hợp PTTM </li></p>
+            <p><li><b>Giảm 50%</b> Tẩy trắng răng Laser Whitening/ Trám răng thẩm mỹ/ Nhổ răng cho khách hàng PTTM</li></p>
+            <p><li><b>Giảm thêm 5%</b> Cấy mỡ toàn mặt / Căng chỉ toàn mặt / Trẻ hóa Mi Young khi thực hiện tiểu phẫu, đại phẫu</li></p>
+            <p><li><b>Tặng 1 buổi chăm sóc da miễn phí</b> (điện di C/ điện di cho khách hàng đăng ký dịch vụ VIP)</li></p>
         </div>
     </div>
 </div>
